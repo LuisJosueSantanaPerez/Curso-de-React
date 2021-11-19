2ecbe87c06eed890e109c1c87e743cf5a235dc32
Luis Josue Santana <josuesantana17@gmail.com>
2021-11-19 15:44:55 -0400

    iniciando proyecto git expert app

---
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -0,0 +1,10 @@
+const GifExpertApp  = () => {
+    return (
+      <>
+          <h2>GifExpertApp</h2>
+          <hr/>
+      </>
+    );
+}
+
+export default GifExpertApp;
---
+++ 04-gif-expert-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import GifExpertApp from "./GifExpertApp";
+
+ReactDOM.render(
+  <GifExpertApp />,
+  document.getElementById('root')
+);
