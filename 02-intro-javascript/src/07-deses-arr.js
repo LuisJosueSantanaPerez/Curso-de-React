5d90ed8ee5e757bc730bd2b21f7b9093124860d7
Luis Josue Santana <josuesantana17@gmail.com>
2021-11-19 12:03:06 -0400

    introduccion a javascript

---
+++ 02-intro-javascript/src/07-deses-arr.js
@@ -0,0 +1,20 @@
+
+
+const personajes = ['Goku','Vegeta','Trunks'];
+const [ , , p3 ] = personajes;
+
+export const retornaArreglo = () =>{
+    return ['ABC', 123];
+}
+
+// const [ letras, numeros ] = retornaArreglo();
+
+// Tarea
+// 1. el primer valor del arr se llamará nombre
+// 2. se llamará setNombre
+const usState = ( valor ) => {
+    return [ valor, ()=>{ console.log('Hola Mundo') } ];
+}
+
+
+
---
+++ 02-intro-javascript/src/README.md
@@ -0,0 +1,5 @@
+# Curso de React
+
+## 01-intro-react
+
+## 02-intro-javascript
