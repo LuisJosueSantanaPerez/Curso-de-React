bf728b6e4955722e5827d2f99c3c6efe11ec1168
Luis Josue Santana <josuesantana17@gmail.com>
2021-11-25 15:56:10 -0400

    agregando prueba del helpers getGifs.js

---
+++ 04-gif-expert-app/src/tests/helpers/getGifs.test.js
@@ -0,0 +1,13 @@
+import {getGifs} from "../../helpers/getGifs";
+
+describe('Pruebas con getGifs Fecth', ()=>{
+    test("debe de traer 10 elemento", async () => {
+        const gifs = await getGifs('One Punch');
+        expect(gifs.length).toBe(10);
+    });
+
+    test("debe de traer nada", async () => {
+        const gifs = await getGifs('');
+        expect(gifs.length).toBe(0);
+    });
+});
